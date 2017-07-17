a5edeb88c008eb08a647a867b894610fc90101bc
Phillip Gray <phillipgray@Alice-Bucks-MacBook.local>
2017-07-17 06:58:44 -0400

    initial scaffolding and vuex setup

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="home">
+  {{ msg }}
+</div>
+</template>
+
+<script>
+import {
+  mapGetters
+} from 'vuex'
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+     ...mapGetters({})
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/store/store.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+  deck: [
+    {
+      question: "How many planets are there in the solar system?",
+      answer: "8 or 9",
+      attempts: 0,
+      correct: 0,
+      mod: "2017-07-16T19:28:59.587Z"
+    },
+    {
+      question: "How many countries are there in the world?",
+      answer: "196",
+      attempts: 0,
+      correct: 0,
+      mod: "2017-07-16T19:29:29.586Z"
+    },
+    {
+      question: "",
+      answer: "",
+      attempts: 0,
+      correct: 0,
+      mod: ""
+    }
+  ]
+  }
+})
